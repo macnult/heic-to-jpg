03ccadfbdec5094c0c912a1d5795cceafd7e6808
macnult <113482585+macnult@users.noreply.github.com>
2024-03-13 11:33:14 -0400

    Add project files.

---
+++ FormsApp/Form1.Designer.cs
@@ -0,0 +1,87 @@
+﻿namespace FormsApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.convert = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(144, 108);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(137, 54);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Select File(s)";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(378, 122);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(258, 26);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // convert
+            // 
+            this.convert.Location = new System.Drawing.Point(297, 293);
+            this.convert.Name = "convert";
+            this.convert.Size = new System.Drawing.Size(146, 60);
+            this.convert.TabIndex = 2;
+            this.convert.Text = "Convert";
+            this.convert.UseVisualStyleBackColor = true;
+            this.convert.Click += new System.EventHandler(this.convert_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.convert);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button convert;
+    }
+}
+
---
+++ FormsApp/Form1.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FormsApp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+
+            openFileDialog.Filter = "HEIC Files (*.heic)|*.heic";
+            openFileDialog.Multiselect = true;
+
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                string[] filePaths = openFileDialog.FileNames;  // Array of filenames
+
+                // Clear the textbox before displaying new filenames (optional)
+                textBox1.Text = "";
+
+                bool isFirstFile = true;  // Flag to track the first file
+
+                // Loop through filenames and display them in the textbox
+                foreach (string filePath in filePaths)
+                {
+                    string fileName = Path.GetFileName(filePath);
+
+                    // Formatting each file name
+                    if (isFirstFile)
+                    {
+                        textBox1.Text += "\"" + fileName;
+                        isFirstFile = false;
+                    }
+                    else
+                    {
+                        
+                        textBox1.Text += ", \"" + fileName;
+                    }
+                }
+                textBox1.Text += "\"";
+            }
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            // i think the convert script would go here
+        }
+
+        private void convert_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
