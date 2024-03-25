8cf9123c6bd5a806a74ea723e1f67e3322ec4577
macnult <113482585+macnult@users.noreply.github.com>
2024-03-25 16:12:19 -0400

    Remove unused libraries

--- FormsApp/Form1.cs
+++ FormsApp/Form1.cs
@@ -1,15 +1,8 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
 using System.IO;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 using ImageMagick;
-using static System.Windows.Forms.VisualStyles.VisualStyleElement.Window;
+
 
 namespace FormsApp
 {
