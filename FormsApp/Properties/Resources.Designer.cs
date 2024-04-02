3a4be43216c7849194bc29efd54ddfbd4383995c
macnult <113482585+macnult@users.noreply.github.com>
2024-04-02 10:42:22 -0400

    Change project name

--- FormsApp/Properties/Resources.Designer.cs
+++ FormsApp/Properties/Resources.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace FormsApp.Properties {
+namespace FileConvert.Properties {
     using System;
     
     
@@ -39,7 +39,7 @@ namespace FormsApp.Properties {
         internal static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("FormsApp.Properties.Resources", typeof(Resources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("FileConvert.Properties.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
